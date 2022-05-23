6e570cb06d12abab5fc6ad7b0c643a68c0029f0c
Darius Gronenborn <darius42@posteo.de>
2022-05-23 11:09:03 +0200

    changed line commands in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,7 @@ monate = ('january', 'february', 'march', 'april', 'may', 'june', 'all')
 tage = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'all')
 staedte = ('2', '1','3')
 def get_filters():
-    
+
     """
     Asks user to specify a city, month, and day to analyze.
 
@@ -19,7 +19,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    
+
     citiesRead = True
     #  get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while citiesRead:
@@ -42,19 +42,19 @@ def get_filters():
 
     weekRead = True
              #get user input for day of week (all, monday, tuesday, ... sunday)
-    while weekRead: 
+    while weekRead:
         day = input('\nWhich day are you interested in? Please enter a day from Monday - Sunday, or all\n').lower()
         if day not in tage:
             print('\nWhich kind of day is that? Please enter a valid day, or all:\n')
             continue
         else:
             weekRead = False
-            
+
     print('-'*40)
     return city, month, day
 
 def load_data(city, month, day):
-                        
+
                              #Loads data for the specified city and filters by month and day if applicable.
                             # Args:
                              #   (str) city - name of the city to analyze
@@ -62,47 +62,47 @@ def load_data(city, month, day):
                              #   (str) day - name of the day of week to filter by, or "all" to apply no day filter
                             #Returns:
                               #  df - Pandas DataFrame containing city data filtered by month and day
-                                        
-                        
+
+
     #load selected data from the csv
-    
+
     df = pd.read_csv(CITY_DATA[city])
-    #df = pd.read_csv('{}.csv'.format(city)) 
-    
+    #df = pd.read_csv('{}.csv'.format(city))
+
 #change the 'Start Time' into datetime and geting the month and the day out and put those in a new column
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['month'] = df['Start Time'].dt.month
     df['day'] = df['Start Time'].dt.weekday_name
-                 
+
 # if user choose not all in monate--> get the month
     if month != 'all':
         monate = ['january', 'february', 'march', 'april', 'may', 'june']
-        month = monate.index(month) + 1             
-        df = df[df['month'] == month]  
-                 
+        month = monate.index(month) + 1
+        df = df[df['month'] == month]
+
 # if user choose not all in Tage--> get the day
     if day != 'all':
         df = df[df['day'] == day.title()]
-                 
-                 
-    return df     
-                 
-                
-def time_stats(df):  
+
+
+    return df
+
+
+def time_stats(df):
             #Displays statistics on the most frequent times of travel.
     print('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()             
-                 
+    start_time = time.time()
+
             #display the most common month
     popul_month = df['month'].mode()[0]
     print('the most common month is: ', popul_month)
-                 
+
            #display the most common day of week
     popul_day = df['day'].mode()[0]
-    print('the most common day is: ', popul_day)    
-                 
+    print('the most common day is: ', popul_day)
+
            #display the most common start hour
-                          
+
     df['hour']=df['Start Time'].dt.hour # kann das weg? -> nein leider nicht definiert die Stunden aus de rstartzeit
     popul_hour = df['hour'].mode()[0]
     print('the most common hour is: ', popul_hour)
@@ -135,7 +135,7 @@ def station_stats(df):
 
 def trip_duration_stats(df):
             #Displays statistics on the total and average trip duration.
-    
+
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
@@ -159,14 +159,14 @@ def user_stats(df):
             #Displays counts of user types
     user_types = df['User Type'].value_counts()
     print('User Types:\n', user_types)
-    
 
-             # Displayc counts of gender, 
+
+             # Displayc counts of gender,
     try:
         user_sex =df['Gender'].value_counts()
         print('User_Gender:\n', user_sex) # No diverse ??? Nowadays it should contain diverse as well !!!
     except:
-        print('No Gender info in this city') 
+        print('No Gender info in this city')
 
          #Displays earliest, most recent, and most common year of birth
     try:
@@ -176,7 +176,7 @@ def user_stats(df):
         print('The youngest user was born in: ', jung)
         mitte = df['Birth Year'].mode()[0]
         print('The most common users birthyear is: ', mitte)
-    except: 
+    except:
         print('No birth data avalable in this city')
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -187,21 +187,20 @@ def raw_data(df):
     read_antwort = True
     antwort = input('Are you interested in viewing the first 5 rows of the raw data? Enter y/n \n').lower()
     fortytwo = 0
-    while read_antwort: 
+    while read_antwort:
          if antwort == 'n':
             read_antwort = False
          if antwort == '42':
             print('It looks like you found the answer to the answer of life, the universe, and everything') # one easteregg should be inside! right?
             read_antwort = False
          if antwort == 'y':
-            print(df[fortytwo:fortytwo+5]) 
+            print(df[fortytwo:fortytwo+5])
             antwort=input('there are another 5 rows available... Print? y/n\n').lower()
-            fortytwo += 5         
-         #else:
-          #  Print('that is actually the wrong answer')
-                          
+            fortytwo += 5
+         # removed "else" -> it was about the wrong answer means not "42"
+
 def main():
-    
+
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
@@ -210,7 +209,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        raw_data(df)    
+        raw_data(df)
 
         restart = input('\nWould you like to restart? Enter y or n.\n')
         if restart.lower() != 'y':
@@ -219,4 +218,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
+#Thanks for all the fish
